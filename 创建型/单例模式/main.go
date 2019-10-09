aab60ca6371e0298c2f60f9f2510d1ef1e8ba85c
kyang <yk1106>
2019-10-09 09:26:45 +0800

    first README

--- 创建型/单例模式/main.go
+++ 创建型/单例模式/main.go
@@ -2,15 +2,40 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 
 	"design-mode/创建型/单例模式/singleton"
 )
 
-func main() {
+func main1() {
 	instance_1 := singleton.GetInstance()
-	instance_1["this"] = "that"
 
 	instance_2 := singleton.GetInstance()
-	s := instance_2["this"]
-	fmt.Println(s)
+	fmt.Printf("instance_1=%p\ninstance_2=%p", instance_1, instance_2)
+	//fmt.Println(s)
+}
+
+var once sync.Once
+
+func main() {
+
+	for i, v := range make([]string, 10) {
+		once.Do(onces)
+		fmt.Println("count:", v, "---", i)
+	}
+	for i := 0; i < 10; i++ {
+
+		go func() {
+			once.Do(onced)
+			fmt.Println("213")
+		}()
+	}
+	time.Sleep(4000)
+}
+func onces() {
+	fmt.Println("onces")
+}
+func onced() {
+	fmt.Println("onced")
 }
--- 创建型/单例模式/singleton/singleton.go
+++ 创建型/单例模式/singleton/singleton.go
@@ -3,17 +3,19 @@ package singleton
 import "sync"
 
 //	定义一个对象类型，该类型只有唯一的对象
-type singleton map[string]string
+type singleton struct {
+	Name string
+}
 
 var (
 	once sync.Once //	保证一个函数只运行一次
 
-	instance singleton //	被确保唯一的对象
+	instance *singleton //	被确保唯一的对象
 )
 
-func GetInstance() singleton {
+func GetInstance() *singleton {
 	once.Do(func() {
-		instance = make(singleton)
+		instance = new(singleton)
 	})
 	return instance
 }
